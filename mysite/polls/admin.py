b6255a68f403aa655ed373bf2c7cc23d9ac63b4e
Kwonill Kim <kikim@delcam.co.kr>
2016-09-10 19:32:45 +0900

    customize admin and its templates

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    max_num = 3
+    min_num = 0
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date', 'choice']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
